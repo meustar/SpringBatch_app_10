0c874af95487ad6c3d4c75c4a7847181b88b011a
meustar <meustar829@gmail.com>
2024-10-23 10:47:10 +0900

    스프링 시큐리티, 로그인, 로그아웃, 회원가입2

---
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/home/HomeController.java
@@ -0,0 +1,10 @@
+package com.koreait.exam.springbatch_app_10.app.home;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String showMain() {
+        return "home/main";
+    }
+}

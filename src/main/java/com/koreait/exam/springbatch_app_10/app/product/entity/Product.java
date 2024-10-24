ed5fbcfb23c66bcb31e4d969908eb66558b3a3c2
meustar <meustar829@gmail.com>
2024-10-24 14:02:09 +0900

    주문 상세 페이지 구현

--- src/main/java/com/koreait/exam/springbatch_app_10/app/product/entity/Product.java
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/product/entity/Product.java
@@ -18,10 +18,28 @@ import static lombok.AccessLevel.PROTECTED;
 @SuperBuilder
 @NoArgsConstructor(access = PROTECTED) // 아무런 매개변수가 없는 생성자를 생성, 다른 패키지에 소속된 클래스는 접근 불허 -> 보안상 좋다
 public class Product extends BaseEntity {
+
     private String title;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Member author;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Song song;
+
     private int price;
+
+    public Product(long id) {
+        super(id);
+    }
+    public int getSalePrice() {
+        return getPrice();
+    }
+    public int getWholesalePrice() {
+        return (int) Math.ceil(getPrice() * 0.7);
+    }
+    public boolean isOrderable() {
+        return true;
+    }
+
 }

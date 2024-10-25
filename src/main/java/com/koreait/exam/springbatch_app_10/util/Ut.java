8094afee882c372588db9df9899a441cd97fa849
meustar <meustar829@gmail.com>
2024-10-25 13:04:25 +0900

    입력된 연 월에 해당하는 날짜 범위 구하기

--- src/main/java/com/koreait/exam/springbatch_app_10/util/Ut.java
+++ src/main/java/com/koreait/exam/springbatch_app_10/util/Ut.java
@@ -1,4 +1,5 @@
 package com.koreait.exam.springbatch_app_10.util;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.koreait.exam.springbatch_app_10.app.AppConfig;
@@ -6,11 +7,38 @@ import com.koreait.exam.springbatch_app_10.app.base.dto.RsData;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+
 public class Ut {
+
+    public static class date {
+        public static int getEndDayOf(int year, int month) {
+            String yearMonth = year + "-" + "%02d".formatted(month);
+            return getEndDayOf(yearMonth);
+        }
+        public static int getEndDayOf(String yearMonth) {
+            LocalDate convertedDate = LocalDate.parse(yearMonth + "-01", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            convertedDate = convertedDate.withDayOfMonth(
+                    convertedDate.getMonth().length(convertedDate.isLeapYear()));
+            return convertedDate.getDayOfMonth();
+        }
+        public static LocalDateTime parse(String pattern, String dateText) {
+            return LocalDateTime.parse(dateText, DateTimeFormatter.ofPattern(pattern));
+        }
+        public static LocalDateTime parse(String dateText) {
+            return parse("yyyy-MM-dd HH:mm:ss.SSSSSS", dateText);
+        }
+    }
+
+
     private static ObjectMapper getObjectMapper() {
         return (ObjectMapper) AppConfig.getContext().getBean("objectMapper");
     }
